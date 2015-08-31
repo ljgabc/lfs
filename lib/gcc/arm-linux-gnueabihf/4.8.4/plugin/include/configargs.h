6ef3bcae530607c8203fc42b97293800430a9318
ljgabc <ljgabc@gmail.com>
2015-08-31 09:55:04 +0800

    Squashed 'tools/' changes from 9cfddfc..8535867
    8535867 add envsetup.sh
    b5e0bef Merge branch 'master' of github.com:ljgabc/tools
    5b51955 initial with gcc-4.8.4, glibc-28.4, glibc-2.19, binutils-2.24, make-4.0
    
    git-subtree-dir: tools
    git-subtree-split: 8535867ce53b6c4a3f4f767bcbf92eb37a3e7964

---
+++ lib/gcc/arm-linux-gnueabihf/4.8.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.4/configure --prefix=/tools --build=x86_64-cross-linux-gnu --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf --disable-nls --enable-languages=c,c++ --disable-multilib -with-mpfr-include=/home/lijg/work/gcc-build/../gcc-4.8.4/mpfr/src --with-mpfr-lib=/home/lijg/work/gcc-build/mpfr/src/.libs --disable-static --enable-__cxa_atexit --enable-threads=posix --enable-checking=release --enable-libstdcxx-time --with-system-zlib --with-local-prefix=/tools --with-native-system-header-dir=/tools/include";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "tls", "gnu" } };
