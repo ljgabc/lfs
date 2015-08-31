372f2f690f1a4d78d2a4349ade6451801579d1bd
ljgabc <ljgabc@gmail.com>
2015-08-31 17:13:08 +0800

    update cflags ldflags

--- tools/envsetup.sh
+++ tools/envsetup.sh
@@ -2,3 +2,8 @@
 
 export PATH="/tools/bin:$PATH"
 
+export CPPFLAGS="-mfloat-abi=hard"
+export CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=neon -mthumb -O3 -I/usr/include"
+export CXXFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=neon -mthumb -O3"
+export LDFLAGS="-march=armv7-a -L/usr/lib"
+
